72a0164cd48cef8360bd28075d1d5934f3362104
qwetuo <chewminzhuang@hotmail.com>
2019-01-25 22:14:31 +0800

    added searchbar with search and undo button that toggles note display

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const searchBarElem = () => {
+  const searchContainer = document.createElement("div")
+  searchContainer.setAttribute("id", "search-container")
+
+  const input = document.createElement("input")
+  input.setAttribute("id", "search-input")
+  input.setAttribute("placeholder", "Search for note title...")
+  searchContainer.appendChild(input)
+
+  const searchBtn = document.createElement("button")
+  searchBtn.textContent = "Search"
+  searchBtn.classList.add("search-button")
+  searchContainer.appendChild(searchBtn)
+  searchBtn.addEventListener("click", event => {
+    const searchValue = input.value.toLowerCase()
+
+    const notes = document.querySelectorAll(".note-container")
+
+    notes.forEach(note => note.style.display = "block")
+
+    const notesWithNoMatch = Array.from(notes).filter(note => {
+      const titleValue = note.querySelector(".title").value.toLowerCase()
+      return titleValue.indexOf(searchValue) === -1
+    })
+    notesWithNoMatch.forEach(note => note.style.display = "none")
+  })
+
+  const undoBtn = document.createElement("button")
+  undoBtn.textContent = "Undo"
+  undoBtn.classList.add("undo-button")
+  searchContainer.appendChild(undoBtn)
+  undoBtn.addEventListener("click", event => {
+    const notes = document.querySelectorAll(".note-container")
+    notes.forEach(note => note.style.display = "block")
+  })
+
+  return searchContainer
+}
+
+document.body.appendChild(searchBarElem())
+
 const createNewNote = () => {
   const note = document.createElement("div");
   note.classList.add("note-container");
